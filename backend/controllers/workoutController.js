8a4ccfa0bc57dc711f61de058d9c1b53ab860985
Hachimnankira <kiran9860819025@gmail.com>
2023-10-06 21:44:07 +0545

    form validation

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -24,6 +24,22 @@ const getWorkout = async (req, res) =>{
 //create a new workout
 const createWorkout = async (req, res) => {
     const { title, reps, load } = req.body
+
+    let emptyFields = []
+
+    if(!title) {
+        emptyFields.push('title')
+    }
+    if(!reps) {
+        emptyFields.push('reps')
+    }
+    if(!load) {
+        emptyFields.push('load')
+    }
+    if(emptyFields.length > 0 ){
+        return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+    }
+
     //add doc to DB
     try {
         const workout = await Workout.create({ title, reps, load })
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -8,6 +8,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState('')
   const [reps, setReps] = useState('')
   const [error, setError] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -25,8 +26,10 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error)
+      setEmptyFields(json.emptyFields)
     }
     if (response.ok) {
+      setEmptyFields([])
       setError(null)
       setTitle('')
       setLoad('')
@@ -46,6 +49,7 @@ const WorkoutForm = () => {
         type="text" 
         onChange={(e) => setTitle(e.target.value)} 
         value={title}
+        className={ emptyFields.includes('title') ? 'error' : ''}
       />
 
       <label>Load (in kg):</label>
@@ -53,6 +57,7 @@ const WorkoutForm = () => {
         type="number" 
         onChange={(e) => setLoad(e.target.value)} 
         value={load}
+        className={ emptyFields.includes('load') ? 'error' : ''}
       />
 
       <label>Number of Reps:</label>
@@ -60,6 +65,7 @@ const WorkoutForm = () => {
         type="number" 
         onChange={(e) => setReps(e.target.value)} 
         value={reps} 
+        className={ emptyFields.includes('reps') ? 'error' : ''}
       />
 
       <button>Add Workout</button>
